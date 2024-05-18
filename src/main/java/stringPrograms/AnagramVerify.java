39b5b2507b950176fab743d7b6bd0795718eaaea
lokeshkalal217 <lokesh.kalal217@gmail.com>
2024-05-18 20:34:16 +0530

    Added program for checking string anagram

--- src/main/java/stringPrograms/AnagramVerify.java
+++ src/main/java/stringPrograms/AnagramVerify.java
@@ -1,24 +1,31 @@
 package stringPrograms;
 
 public class AnagramVerify {
-	
-	public static void main(String[] args) {
-		
-		String s1 = "Mother in Law";
-		String s2 =  "Hitler Woman";
-		
-		
-		String copyOfs1 = s1.replaceAll("\\s", "");
 
-		String copyOfs2 = s2.replaceAll("\\s", "");
-		
-		System.out.println(copyOfs1);
-		System.out.println(copyOfs2);
-		
-		if(copyOfs1.toLowerCase().length()==copyOfs2.toLowerCase().length()) {
-			
-			
+	public static void main(String[] args) {
+
+		String s1 = "Mother in Law";
+		String s2 = "Hitler Woman";
+
+		boolean b = isAnagram(s1, s2);
+		System.out.println(b ? "Anagram" : "Not Anagram");
+
+	}
+
+	public static boolean isAnagram(String a, String b) {
+
+		if (a.replaceAll("\\s", "").length() != b.replaceAll("\\s", "").length()) {
+			return false;
+
 		}
+
+		char[] first = a.toLowerCase().replaceAll("\\s", "").toCharArray();
+		char[] second = b.toLowerCase().replaceAll("\\s", "").toCharArray();
+
+		java.util.Arrays.sort(first);
+		java.util.Arrays.sort(second);
+
+		return java.util.Arrays.equals(first, second);
 
 	}
 
